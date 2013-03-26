5b415f46476249346fc06f6b5a29b0f3af4040fa
siva <siva.omer@auis.edu.iq>
2013-03-26 04:05:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssiApp::Application.config.secret_token = '8b0f5a416df615fb1db7ab63449783ae702d4397251bf0f63449caeb6b5110668fecb9855676fda0db16e0e3e4e6a611758b3f5faf13c0f13869f1b02f819fa1'
